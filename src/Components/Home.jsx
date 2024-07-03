7ac6b6f45ec7786fbb45455f1e095de1c4ad9063
Sarbajit Acharjee <87385430+sarbajitacharjee@users.noreply.github.com>
2024-07-03 10:31:47 +0000

    k

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,6 +13,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
+ // eslint-disable-next-line prettier/prettier
  * directory and import here for use. Then, set imageAltText to string that 
  * represents what you see in that image.
  *
