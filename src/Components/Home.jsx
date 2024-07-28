0e9631c2ad795dd6be9ce5d615dc6af8c699dbcc
Sarbajit Acharjee <87385430+sarbajitacharjee@users.noreply.github.com>
2024-07-28 05:24:56 +0000

    commit all

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -23,6 +23,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/mypicture1.webp";
 
+// eslint-disable-next-line prettier/prettier
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
